20ab2775b39c53df5569550a1bebbb9cbd3cf2bc
Dyneteve <dyneteve@pixelexperience.org>
2020-02-28 13:37:01 +0000

    violet: Update bluetooth stack from coral
    * It also includes FM/ANT

--- device.mk
+++ device.mk
@@ -136,14 +136,6 @@ BOOT_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
