db19c902de961a117feac430779ae3475f9310df
Dyneteve <dyneteve@pixelexperience.org>
2020-02-28 14:03:40 +0000

    violet: Import system proprieties from coral
    * Also keep DSDS enabled

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -29,18 +29,23 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio_hal.period_size=192 \
     vendor.voice.path.for.pcm.voip=true
 
-# Bluetooth
+# Audio fluence, ns, aec property, voice and media volume steps
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptive \
-    persist.vendor.bt.aac_frm_ctl.enabled=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecomm=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    ro.config.vc_call_vol_steps=7 \
+    ro.config.media_vol_steps=25
+
+# Bluetooth SoC
+PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qcom.bluetooth.soc=cherokee
 
-# CNE and DPM
+# Bluetooth WiPower
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.cne.feature=1 \
-    persist.vendor.dpm.feature=1 \
-    persist.vendor.dpm.nsrm.bkg.evt=3955 \
-    ro.vendor.use_data_netmgrd=true
+    ro.vendor.bluetooth.emb_wp_mode=false \
+    ro.vendor.bluetooth.wipower=false
 
 # Enable Codec 2.0
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -72,10 +77,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_default_color_mode=0 \
     vendor.gralloc.disable_ubwc=0
 
-# DRM
-PRODUCT_PROPERTY_OVERRIDES += \
-    drm.service.enabled=true
-
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp
@@ -84,37 +85,53 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore_desede=true
 
-# Netmgr
+# Keyguard
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.data.iwlan.enable=true \
-    persist.vendor.data.mode=concurrent
+    keyguard.no_require_sim=true
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so
 
-# Qualcomm / OEM Unlock
+# OEM Unlock
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.oem_unlock_supported=1 \
-    ro.vendor.qti.va_aosp.support=1
+    ro.oem_unlock_supported=1
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds \
+    persist.vendor.cne.feature=1 \
+    persist.vendor.data.iwlan.enable=true \
+    persist.vendor.radio.RATE_ADAPT_ENABLE=1 \
+    persist.vendor.radio.ROTATION_ENABLE=1 \
+    persist.vendor.radio.VT_ENABLE=1 \
+    persist.vendor.radio.VT_HYBRID_ENABLE=1 \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
-    persist.vendor.radio.atfwd.start=true \
     persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.enable_temp_dds=true \
-    persist.vendor.radio.enableadvancedscan=true \
-    persist.vendor.radio.force_on_dc=true \
-    persist.vendor.radio.procedure_bytes=SKIP \
-    persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.redir_party_num=1 \
-    persist.vendor.radio.report_codec=1 \
+    persist.vendor.radio.data_ltd_sys_ind=1 \
+    persist.vendor.radio.videopause.mode=1 \
+    persist.vendor.radio.multisim_switch_support=true \
     persist.vendor.radio.sib16_support=1 \
-    ril.subscription.types=NV,RUIM \
-    ro.telephony.default_network=22,22 \
-    telephony.lteOnCdmaDevice=1
+    persist.vendor.radio.data_con_rprt=true \
+    persist.vendor.radio.relay_oprt_change=1 \
+    persist.vendor.radio.no_wait_for_card=1 \
+    persist.vendor.radio.sap_silent_pin=1 \
+    persist.rcs.supported=1 \
+    vendor.rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
+    persist.vendor.radio.procedure_bytes=SKIP
+
+# Radio - Set default CDMA subscription to RUIM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.default_cdma_sub=0
+
+# Radio - Disable snapshot timer
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.snapshot_enabled=0 \
+    persist.vendor.radio.snapshot_timer=0
+
+# Subsystem silent restart
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.sys.ssr.restart_level=modem,slpi,adsp
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
@@ -125,9 +142,17 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
+# SurfaceFlinger - Enable backpressure for GL comp
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.enable_gl_backpressure=1 \
+    debug.sf.enable_gl_backpressure=1
+
+# SurfaceFlinger - Enable EGL image tracking on SF for b/137514000
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_egl_image_tracker=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_phase_offset_ns=500000 \
     debug.sf.early_app_phase_offset_ns=500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000
+
