6a38a419de2b8f0320a83325e3117aa6106f8a4f
Dyneteve <dyneteve@pixelexperience.org>
2020-03-06 19:22:12 +0200

    violet: Drop overlay-pe

--- device.mk
+++ device.mk
@@ -27,8 +27,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay
 
 # Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
