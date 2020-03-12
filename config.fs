b26e5414874a195eb58137051c8b32d6c84b67c8
Dyneteve <dyneteve@pixelexperience.org>
2020-03-12 16:06:51 +0200

    violet: Address denials and cleanup
    * Drop contained modules ( Add the ims deamons to ramdisk )
    * Add ipacm folder
    * Move to diag instead of vendor_qti_diag
    * Import config.fs from coral
    * Address denials and make them preatty
    * Add lowi-server

--- config.fs
+++ config.fs
@@ -1,5 +1,3 @@
-[AID_VENDOR_QTI_DIAG]
-value:2901
 
 [AID_VENDOR_QDSS]
 value:2902
@@ -16,34 +14,19 @@ value:2905
 [AID_VENDOR_QRTR]
 value:2906
 
-[AID_VENDOR_THERMAL]
-value:2907
-
-[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND NET_ADMIN
-
 [vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[vendor/bin/pd-mapper]
+[vendor/bin/cnss-daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
 [vendor/bin/imsdatadaemon]
-mode: 0755
-user: AID_RADIO
-group: AID_RADIO
-caps: NET_BIND_SERVICE
-
-[vendor/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_RADIO
 group: AID_RADIO
@@ -57,14 +40,14 @@ caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [vendor/bin/cnd]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
-[vendor/bin/slim_daemon]
+[vendor/bin/ims_rtp_daemon]
 mode: 0755
-user:  AID_GPS
-group: AID_GPS
+user: AID_RADIO
+group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [vendor/bin/loc_launcher]
@@ -73,14 +56,8 @@ user:  AID_GPS
 group: AID_GPS
 caps: SETUID SETGID
 
-[vendor/bin/sensors.qti]
+[system/vendor/bin/loc_launcher]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[vendor/firmware_mnt/image/*]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
