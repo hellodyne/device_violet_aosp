964a0b890d2fb8fe98ec6fdf7c5b5a3d370e3774
Dyneteve <dyneteve@pixelexperience.org>
2020-03-18 19:05:21 +0200

    violet: Import seecomp policy from coral

--- device.mk
+++ device.mk
@@ -361,7 +361,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
