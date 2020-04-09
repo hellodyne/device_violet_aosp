fc7061c82f253419fc8b67edbe074ab4ddac7d5c
Dyneteve <sound0020@gmail.com>
2020-04-09 17:49:47 +0300

    violet: Build custom composer service
    * The android.hardware.graphics.composer@2.3-service-sm8150 inherits from android.hardware.graphics.composer@2.3-service. Use it to enable pps socket.

--- device.mk
+++ device.mk
@@ -161,7 +161,7 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.3-service \
+    android.hardware.graphics.composer@2.3-service-sm8150 \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
